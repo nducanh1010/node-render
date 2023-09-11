c8e6692fbb03fbee068081ae1ddf542327fc4c0a
Duc Anh <nducanh1010@gmail.com>
2023-09-11 13:47:37 +0700

    init

---
+++ build/server.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+var _viewEngine = _interopRequireDefault(require("./config/viewEngine"));
+var _web = _interopRequireDefault(require("./routes/web"));
+var _logger = _interopRequireDefault(require("../logger"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+require("dotenv").config();
+var app = (0, _express["default"])();
+
+//config view Engine
+(0, _viewEngine["default"])(app);
+
+//init all web routes
+(0, _web["default"])(app);
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+  console.log("Node Js app is running at the port ".concat(port));
+});
+app.get("/da", function (req, res) {
+  setTimeout(function () {
+    throw new Error("We crashed!!!!!");
+  }, 10);
+});
---
+++ src/server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+import express from "express";
+import configViewEngine from "./config/viewEngine";
+import initWebRoutes from "./routes/web";
+import logger from "../logger";
+let app = express();
+
+//config view Engine
+configViewEngine(app);
+
+//init all web routes
+initWebRoutes(app);
+
+let port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Node Js app is running at the port ${port}`);
+});
+app.get("/da", (req, res) => {
+  setTimeout(function () {
+    throw new Error("We crashed!!!!!");
+  }, 10);
+});
